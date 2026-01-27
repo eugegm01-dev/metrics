bce210d8e01c6add693af186d44a49f1ce234373
Gubaev_Evgeniy_Maratovich <euge-g@bk.ru>
2026-01-27 07:43:57 +0300

    Fix: Use single migration file for Goose compatibility

--- migrations/001_create_tables.sql
+++ migrations/001_create_tables.sql
@@ -1,3 +1,4 @@
+-- +goose Up
 CREATE TABLE IF NOT EXISTS gauges (
     name VARCHAR(255) PRIMARY KEY,
     value DOUBLE PRECISION
@@ -7,3 +8,6 @@ CREATE TABLE IF NOT EXISTS counters (
     name VARCHAR(255) PRIMARY KEY,
     value BIGINT
 );
+-- +goose Down
+DROP TABLE IF EXISTS counters;
+DROP TABLE IF EXISTS gauges;
